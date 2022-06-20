9afe2cd364d4aae7c3d1ad061cec3e93e8cdd3d2
Joseph Akpu <63398509+akpu-jo@users.noreply.github.com>
2022-06-20 17:12:35 +0100

    Search result 2

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,5 +1,5 @@
 import Recommendations from "@/components/explore/Recommendations";
-import SearchResult from "@/components/explore/searchResult.js";
+import SearchResult from "@/components/explore/SearchResult";
 import MobileNav from "@/components/navs/MobileNav";
 import SearchHeader from "@/components/navs/SearchHeader";
 import React, { useState } from "react";
