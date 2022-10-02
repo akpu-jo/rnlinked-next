c8f0be31a61a51efcb60294cf500e90f5cf38519
Joseph Akpu <63398509+akpu-jo@users.noreply.github.com>
2022-10-02 23:32:49 +0100

    firbase start

--- contexts/AuthContext.js
+++ contexts/AuthContext.js
@@ -3,15 +3,7 @@ import { getAuth, GoogleAuthProvider, TwitterAuthProvider } from 'firebase/auth'
 
 import { createContext, useContext, useEffect, useState} from 'react'
 
-const firebaseConfig = {
-    apiKey: "AIzaSyAsrDblrH558aloxHQ7yKksmJtVenQRbgU",
-    authDomain: "rnlinked-b7321.firebaseapp.com",
-    projectId: "rnlinked-b7321",
-    storageBucket: "rnlinked-b7321.appspot.com",
-    messagingSenderId: "719823973560",
-    appId: "1:719823973560:web:e7da27942fec6b2172184d",
-    measurementId: "G-RDERS51X7W"
-  };
+
   
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
