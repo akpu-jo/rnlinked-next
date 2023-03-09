afe8bc6532ad187959752f390c27b6a834962cbc
Joseph Akpu <63398509+akpu-jo@users.noreply.github.com>
2023-03-09 13:29:54 +0000

    ...

---
+++ pages/admin/index.js
@@ -0,0 +1,37 @@
+import AppBar from "@/layouts/AppBar";
+import User from "@/models/userModel";
+import connectDb from "@/utils/db";
+import React, { useState } from "react";
+import { typeOf } from "react-is";
+import CsvDownloadButton from "react-json-to-csv";
+
+const index = ({u}) => {
+    const users = JSON.parse(u)
+    console.log(typeof u, u)
+  const [usersJson, setUsersJson] = useState("");
+
+  return (
+    <>
+      <AppBar />
+      <CsvDownloadButton data={users}></CsvDownloadButton>
+      <div>welcome to admin</div>
+    </>
+  );
+};
+
+export default index;
+
+export const getServerSideProps = async () => {
+  await connectDb();
+
+  const users = await User.find().select(
+    "_id username name email role createdAt"
+  );
+//   console.log(users);
+
+  return {
+    props: {
+      u: JSON.stringify(users),
+    },
+  };
+};
